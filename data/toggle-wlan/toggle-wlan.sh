4fe865ec373da30f072b20abba16f239f60a3ad0
thedead1440 <ars.2610@gmail.com>
2013-01-23 01:28:27 +0800

    upgrade to v1.0.1

--- data/toggle-wlan/toggle-wlan.sh
+++ data/toggle-wlan/toggle-wlan.sh
@@ -7,9 +7,11 @@ if [ $STATUS == "enabled" ]
 	then
 	dbus-send --print-reply --dest=com.meego.core.MNotificationManager /notificationmanager com.meego.core.MNotificationManager.addNotification uint32:0 uint32:0 string:'device' string:'' string:"WLAN Off" string:'' string:"/home/user/.local/share/icons/hicolor/apps/toggle-wlan-off.png" uint32:0
 	sed -i 's/toggle\-wlan\-on/toggle\-wlan\-off/' /home/user/.local/share/applications/toggle-wlan.desktop
+	gconftool -s --type=bool /system/osso/connectivity/network_type/restricted_mode true
 	`/sbin/mcetool --disable-radio=wlan`
 	else
 	dbus-send --print-reply --dest=com.meego.core.MNotificationManager /notificationmanager com.meego.core.MNotificationManager.addNotification uint32:0 uint32:0 string:'device' string:'' string:"WLAN On" string:'' string:"/home/user/.local/share/icons/hicolor/apps/toggle-wlan-on.png" uint32:0
 	sed -i 's/toggle\-wlan\-off/toggle\-wlan\-on/' /home/user/.local/share/applications/toggle-wlan.desktop
+	gconftool -s --type=bool /system/osso/connectivity/network_type/restricted_mode false
 	`/sbin/mcetool --enable-radio=wlan`
 fi
--- v1.0-1/debian/toggle-wlan/home/user/.local/share/data/toggle-wlan/toggle-wlan.sh
+++ v1.0-1/debian/toggle-wlan/home/user/.local/share/data/toggle-wlan/toggle-wlan.sh
@@ -6,8 +6,10 @@ STATUS=$(/sbin/mcetool --status | grep -v grep | grep WLAN | awk '{print $2}' | 
 if [ $STATUS == "enabled" ]
 	then
 	sed -i 's/toggle\-wlan\-on/toggle\-wlan\-off/' /home/user/.local/share/applications/toggle-wlan.desktop
+	gconftool -s --type=bool /system/osso/connectivity/network_type/restricted_mode true
 	`/sbin/mcetool --disable-radio=wlan`
 	else
 	sed -i 's/toggle\-wlan\-off/toggle\-wlan\-on/' /home/user/.local/share/applications/toggle-wlan.desktop
+	gconftool -s --type=bool /system/osso/connectivity/network_type/restricted_mode false
 	`/sbin/mcetool --enable-radio=wlan`
 fi
